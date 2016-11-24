346df4b98012f3c4ad330082a099a11383741887
Quentin HUSLER <qhusler@e1r12p21.42.fr>
2016-11-24 13:57:11 +0100

    norme sur parse.c

--- parse.c
+++ parse.c
@@ -6,13 +6,13 @@
 /*   By: qhusler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 11:25:03 by qhusler           #+#    #+#             */
-/*   Updated: 2016/11/24 10:49:37 by qhusler          ###   ########.fr       */
+/*   Updated: 2016/11/24 13:56:27 by qhusler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		basic_test(char *file)
+static int		basic_test(char *file)
 {
 	int i;
 	int c;
@@ -39,14 +39,14 @@ int		basic_test(char *file)
 	return (0);
 }
 
-int		medium_test(char *s, t_parse_info *pi, int ntet)
+static int		medium_test(char *s, t_parse_info *pi, int ntet)
 {
 	int i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while (s && s[i] && ntet != 0)
+	while (s && s[i] && ntet--)
 	{
 		i += 20;
 		pi->ndot = 0;
@@ -61,21 +61,20 @@ int		medium_test(char *s, t_parse_info *pi, int ntet)
 		}
 		i++;
 		j++;
-		ntet--;
 		if (pi->ndot != 12 || pi->mdi != 4 || pi->mnl != 4)
 			return (1);
 	}
 	return (0);
 }
 
-int		connect_test(char *s, t_parse_info *pi, int ntet)
+static int		connect_test(char *s, t_parse_info *pi, int ntet)
 {
 	int	i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while (s && s[i] && ntet)
+	while (s && s[i] && ntet--)
 	{
 		i += 20;
 		pi->nb_co = 0;
@@ -83,20 +82,21 @@ int		connect_test(char *s, t_parse_info *pi, int ntet)
 		{
 			pi->nb_co += (s[j] == '#' && s[j + 1] == '#') ? 1 : 0;
 			pi->nb_co += (s[j] == '#' && s[j - 1] == '#') ? 1 : 0;
-			pi->nb_co += (s[j] == '#' && s[j + 5] == '#' && (j + 5) < i) ? 1 : 0;
-			pi->nb_co += (s[j] == '#' && s[j - 5] == '#' && (j - 5) >= (i - 20)) ? 1 : 0;
+			pi->nb_co += (s[j] == '#' && s[j + 5] == '#'
+					&& (j + 5) < i) ? 1 : 0;
+			pi->nb_co += (s[j] == '#' && s[j - 5] == '#'
+					&& (j - 5) >= (i - 20)) ? 1 : 0;
 			j++;
 		}
 		if (pi->nb_co != 6 && pi->nb_co != 8)
 			return (1);
 		i++;
 		j++;
-		ntet--;
 	}
 	return (0);
 }
 
-int		main_parse(t_env *e)
+int				main_parse(t_env *e)
 {
 	t_parse_info	pi;
 
